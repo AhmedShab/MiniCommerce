0bf21cfcfd94d46b2bad4103f7392eb0654e9eeb
Ahmed Shaaban <hello@ahmedshaaban.tech>
2025-07-29 18:03:27 +0300

    get  product

--- models/product.js
+++ models/product.js
@@ -1,27 +1,33 @@
-const Sequelize = require('sequelize');
+const { getDb } = require('../util/database');
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
   }
-});
+
+  save() {
+    const db = getDb();
+
+    db.collection('products')
+      .insertOne(this)
+      .then(result => {
+        console.log('Product saved:', result);
+      })
+      .catch(err => {
+        console.error('Error saving product:', err);
+      });
+  }
+
+  static fetchAll() {
+    // Logic to fetch all products from the database
+  }
+
+  static findById(id) {
+    // Logic to find a product by its ID
+  }
+}
 
 module.exports = Product;
