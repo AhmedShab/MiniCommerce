9964128b707b9c2a54ccf71552d9d8f703927f11
Ahmed Shaaban <hello@ahmedshaaban.tech>
2025-07-29 17:20:18 +0300

    init mongoDb setup

--- util/database.js
+++ util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = cb => {
+  MongoClient.connect('mongodb+srv://ahmed:xr7bfKQ2Qmbf5KS0@cluster0.zzmzliw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(client => {
+      console.log('Connected to MongoDB');
+      cb(client);
+    })
+    .catch(err => {
+      console.error('Failed to connect to MongoDB', err);
+    });
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
