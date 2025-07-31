054b30c36766561ac4ae2d129a90af02c995f06a
Ahmed Shaaban <hello@ahmedshaaban.tech>
2025-07-31 18:21:14 +0300

    save userId ref in product

--- app.js
+++ app.js
@@ -14,21 +14,23 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('6889d18531260941c8baade1')
-//     .then(user => {
-//       if (user) {
-//         req.user = new User(user._id, user.name, user.email, user.cart);
-//       }
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('688b85f75adbf85444f0bffe');
+    if (user) {
+      req.user = user;
+    }
+    next();
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -38,6 +40,19 @@ app.use(errorController.get404);
 async function startServer() {
   try {
     await mongoose.connect('mongodb+srv://ahmed:xr7bfKQ2Qmbf5KS0@cluster0.zzmzliw.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0');
+
+    // only create a new user if it doesn't exist
+    const existingUser = await User.findOne();
+    if (!existingUser) {
+      // create a new user
+      const user = new User({
+        name: 'Ahmed',
+        email: 'ahmed@example.com',
+        cart: { items: [] }
+      });
+      user.save();
+    }
+    // else, use the existing user
     app.listen(3000, () => {
       console.log('Server is running on port 3000');
     });
--- models/user.js
+++ models/user.js
@@ -1,3 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  cart: {
+    items: [{
+      productId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    }]
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
+
 //  /**
 //  * @typedef {Object} Order
 //  * @property {ObjectId} userId
