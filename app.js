e0539415b86a7b34b0be0e5b07f5dc39e238dc48
Ahmed Shaaban <hello@ahmedshaaban.tech>
2025-08-03 13:30:10 +0300

    create basic setup for login

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 const User = require('./models/user');
 
@@ -34,6 +35,7 @@ app.use(async (req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
