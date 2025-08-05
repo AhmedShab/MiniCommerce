ccd812cfac4f986d2f19074b7601762356785044
Ahmed Shaaban <hello@ahmedshaaban.tech>
2025-08-05 20:36:56 +0300

    handle errors

--- app.js
+++ app.js
@@ -42,6 +42,12 @@ app.use(session({
 );
 app.use(csrfProtection);
 app.use(flash());
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 app.use(async (req, res, next) => {
   if (!req.session.user) {
     return next();
@@ -55,31 +61,24 @@ app.use(async (req, res, next) => {
     req.user = user;
     next();
   } catch (err) {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
+    next(new Error(err));
   }
-});
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
 });
 
 app.use('/404', errorController.get404);
 app.use('/500', errorController.get500);
 
-app.use((error, req, res, next) => {
-  if (error.httpStatusCode === 500) {
-    return res.redirect('500');
-  }
-});
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).render('500', {
+    pageTitle: 'Error Occurred',
+    path: '/500',
+  });
+});
 
 async function startServer() {
   try {
